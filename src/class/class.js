030faf74ba70ad5169972d77f5330bf6a404cb06
seranomi <2bumchan@naver.com>
2025-01-09 15:56:34 +0900

    클래스 사용 양식 사용 방법

--- src/class/class.js
+++ src/class/class.js
@@ -29,3 +29,39 @@ console.log(r3.status);
 r1.performTask("Charging");
 r2.performTask("Explorering");
 r3.updateStatus("On Repair");
+// No-args Constructor
+var Pet = /** @class */ (function () {
+    function Pet() {
+        this.category = "Cat";
+        this.name = "Chu";
+        // 기본 생성자
+        //constructor() {}
+    }
+    return Pet;
+}());
+var p1 = new Pet();
+console.log(p1.name);
+var User = /** @class */ (function () {
+    // [생성자 부분]
+    function User(username, email) {
+        this.job = "Student"; // 기본값을 주는 경우
+        this.username = username;
+        this.email = email;
+    }
+    // [메서드 부분]
+    User.prototype.study = function () {
+        console.log("".concat(this.username, " is studying."));
+    };
+    return User;
+}());
+// [객체 생성] (new 키워드와 arguments 입력)
+var user1 = new User("홍길동", "hong@abcd.co.kr");
+var user2 = new User("김철수", "kim@example.com");
+// [객체의 사용, 접근] (. 도트연산자 사용)
+// 1. 인스턴스 자체 접근
+console.log(user1); // 출력 : User{ username: '홍길동', email: '...'}
+// 2. 인스턴스의 세부 필드 접근
+console.log(user2.username); // 출력 : 김철수
+// 3. 메서드 호출
+user1.study();
+user2.study();
--- src/class/class.ts
+++ src/class/class.ts
@@ -34,3 +34,47 @@ console.log(r3.status);
 r1.performTask("Charging");
 r2.performTask("Explorering");
 r3.updateStatus("On Repair");
+
+// No-args Constructor
+class Pet {
+    category: string = "Cat";
+    name: string = "Chu";
+
+    // 기본 생성자
+    //constructor() {}
+}
+
+let p1 = new Pet();
+console.log(p1.name);
+
+class User{
+    // [필드 부분]
+    username: string; // 타입만을 표기하는 경우
+    email: string;
+    job: string = "Student"; // 기본값을 주는 경우
+
+    // [생성자 부분]
+    constructor(username: string, email: string){
+        this.username = username;
+        this.email = email;
+    }
+
+    // [메서드 부분]
+    study(): void {
+        console.log(`${this.username} is studying.`);
+    }
+}
+
+// [객체 생성] (new 키워드와 arguments 입력)
+let user1 = new User("홍길동", "hong@abcd.co.kr");
+let user2 = new User("김철수", "kim@example.com");
+
+// [객체의 사용, 접근] (. 도트연산자 사용)
+// 1. 인스턴스 자체 접근
+console.log(user1) // 출력 : User{ username: '홍길동', email: '...'}
+// 2. 인스턴스의 세부 필드 접근
+console.log(user2.username); // 출력 : 김철수
+// 3. 메서드 호출
+user1.study();
+user2.study();
+
