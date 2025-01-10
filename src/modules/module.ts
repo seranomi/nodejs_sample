4fabdbcff0ae90d90028a82d6faad9e0a805c216
seranomi <2bumchan@naver.com>
2025-01-10 15:21:58 +0900

    타입스크립트의 모듈 export/import

---
+++ src/modules/module.ts
@@ -0,0 +1,8 @@
+import { add, substract, multiply, divide, divide2 } from "./mathOperations";
+
+console.log("더하기 : " + add(1, 2))
+console.log("빼기 : " + substract(1, 2))
+console.log("곱하기 : " + multiply(1, 2))
+console.log("나누기 : " + divide(1, 2))
+console.log("나누기2 : " + divide2(1, 2))
+
